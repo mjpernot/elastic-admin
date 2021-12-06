449222879e0c210c900d193cf6a367c1a1522bf1
Mark Pernot <mark.pernot@gmail.com>
2021-12-06 12:02:46 +0000

    Update test/unit/elastic_db_admin/run_program.py v0.2.9

--- test/unit/elastic_db_admin/run_program.py
+++ test/unit/elastic_db_admin/run_program.py
@@ -42,7 +42,6 @@ def failed_dumps(els, **kwargs):
     Description:  This is a function stub for elastic_db_admin.failed_dumps.
 
     Arguments:
-        els -> Stub argument holder.
 
     """
 
@@ -61,7 +60,6 @@ def list_dumps(els, **kwargs):
     Description:  This is a function stub for elastic_db_admin.list_dumps.
 
     Arguments:
-        els -> Stub argument holder.
 
     """
 
@@ -80,7 +78,7 @@ class ProgramLock(object):
     Description:  Class stub holder for gen_class.ProgramLock class.
 
     Methods:
-        __init__ -> Class initialization.
+        __init__
 
     """
 
@@ -91,13 +89,83 @@ class ProgramLock(object):
         Description:  Class initialization.
 
         Arguments:
-            (input) cmdline -> Argv command line.
-            (input) flavor -> Lock flavor ID.
 
         """
 
         self.cmdline = cmdline
         self.flavor = flavor
+
+
+class CfgTest(object):
+
+    """Class:  CfgTest
+
+    Description:  Class which is a representation of a cfg module.
+
+    Methods:
+        __init__
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the CfgTest class.
+
+        Arguments:
+
+        """
+
+        self.host = ["SERVER_NAME"]
+        self.port = 9200
+        self.user = None
+        self.japd = None
+        self.ssl_client_ca = None
+        self.scheme = "https"
+
+
+class ElasticSearchStatus(object):
+
+    """Class:  ElasticSearchStatus
+
+    Description:  Class stub holder for elastic_class.ElasticSearchStatus class.
+
+    Methods:
+        __init__
+        connect
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.host = "host"
+        self.port = 9200
+        self.user = None
+        self.japd = None
+        self.ca_cert = None
+        self.scheme = "https"
+        self.is_connected = True
+
+    def connect(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        return True
 
 
 class UnitTest(unittest.TestCase):
@@ -107,11 +175,13 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Unit testing initilization.
-        test_raise_exception -> Test with raising exception.
-        test_func_call_multi -> Test run_program with multiple calls.
-        test_func_call_one -> Test run_program with one call to function.
-        test_func_call_zero -> Test run_program with zero calls to function.
+        setUp
+        test_is_not_connected
+        test_is_connected
+        test_raise_exception
+        test_func_call_multi
+        test_func_call_one
+        test_func_call_zero
 
     """
 
@@ -125,35 +195,57 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        class CfgTest(object):
-
-            """Class:  CfgTest
-
-            Description:  Class which is a representation of a cfg module.
-
-            Methods:
-                __init__ -> Initialize configuration environment.
-
-            """
-
-            def __init__(self):
-
-                """Method:  __init__
-
-                Description:  Initialization instance of the CfgTest class.
-
-                Arguments:
-
-                """
-
-                self.host = ["SERVER_NAME"]
-                self.port = 9200
-
         self.cfg = CfgTest()
-
+        self.els = ElasticSearchStatus()
         self.args = {"-c": "config_file", "-d": "config_dir", "-M": True}
         self.func_dict = {"-F": failed_dumps, "-L": list_dumps}
         self.proglock = ProgramLock(["cmdline"], "FlavorID")
+
+    @mock.patch("elastic_db_admin.gen_libs.load_module")
+    @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
+    @mock.patch("elastic_db_admin.gen_class")
+    def test_is_not_connected(self, mock_lock, mock_class, mock_load):
+
+        """Function:  test_is_not_connected
+
+        Description:  Test with failed connection.
+
+        Arguments:
+
+        """
+
+        self.args["-F"] = True
+        self.els.is_connected = False
+
+        mock_lock.return_value = self.proglock
+        mock_class.return_value = self.els
+        mock_load.return_value = self.cfg
+
+        with gen_libs.no_std_out():
+            self.assertFalse(
+                elastic_db_admin.run_program(self.args, self.func_dict))
+
+    @mock.patch("elastic_db_admin.gen_libs.load_module")
+    @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
+    @mock.patch("elastic_db_admin.gen_class")
+    def test_is_connected(self, mock_lock, mock_class, mock_load):
+
+        """Function:  test_is_connected
+
+        Description:  Test with successful connection.
+
+        Arguments:
+
+        """
+
+        self.args["-F"] = True
+
+        mock_lock.return_value = self.proglock
+        mock_class.return_value = self.els
+        mock_load.return_value = self.cfg
+
+        self.assertFalse(
+            elastic_db_admin.run_program(self.args, self.func_dict))
 
     @mock.patch("elastic_db_admin.gen_libs.load_module")
     @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
@@ -172,12 +264,12 @@ class UnitTest(unittest.TestCase):
 
         mock_lock.side_effect = \
             elastic_db_admin.gen_class.SingleInstanceException
-        mock_class.return_value = "ElasticSearch"
+        mock_class.return_value = self.els
         mock_load.return_value = self.cfg
 
         with gen_libs.no_std_out():
-            self.assertFalse(elastic_db_admin.run_program(self.args,
-                                                          self.func_dict))
+            self.assertFalse(
+                elastic_db_admin.run_program(self.args, self.func_dict))
 
     @mock.patch("elastic_db_admin.gen_libs.load_module")
     @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
@@ -197,11 +289,11 @@ class UnitTest(unittest.TestCase):
         self.args["-L"] = True
 
         mock_lock.return_value = self.proglock
-        mock_class.return_value = "ElasticSearch"
+        mock_class.return_value = self.els
         mock_load.return_value = self.cfg
 
-        self.assertFalse(elastic_db_admin.run_program(self.args,
-                                                      self.func_dict))
+        self.assertFalse(
+            elastic_db_admin.run_program(self.args, self.func_dict))
 
     @mock.patch("elastic_db_admin.gen_libs.load_module")
     @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
@@ -219,11 +311,11 @@ class UnitTest(unittest.TestCase):
         self.args["-F"] = True
 
         mock_lock.return_value = self.proglock
-        mock_class.return_value = "ElasticSearch"
+        mock_class.return_value = self.els
         mock_load.return_value = self.cfg
 
-        self.assertFalse(elastic_db_admin.run_program(self.args,
-                                                      self.func_dict))
+        self.assertFalse(
+            elastic_db_admin.run_program(self.args, self.func_dict))
 
     @mock.patch("elastic_db_admin.gen_libs.load_module")
     @mock.patch("elastic_db_admin.elastic_class.ElasticSearchStatus")
@@ -239,11 +331,11 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_lock.return_value = self.proglock
-        mock_class.return_value = "ElasticSearch"
+        mock_class.return_value = self.els
         mock_load.return_value = self.cfg
 
-        self.assertFalse(elastic_db_admin.run_program(self.args,
-                                                      self.func_dict))
+        self.assertFalse(
+            elastic_db_admin.run_program(self.args, self.func_dict))
 
 
 if __name__ == "__main__":
